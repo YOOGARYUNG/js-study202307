8c34cf342e1906d3636549e10b121bb22161eb1d
GaRyung <shubom0115@naver.com>
2023-07-24 15:28:05 +0900

    core js - 변수와상수, 데이터타입, 산술연산자 학습

---
+++ 1. core-js/basic/1. 변수와 상수.js
@@ -0,0 +1,31 @@
+
+// 변수 만들기
+let userName = '뽀로로';
+
+// 재할당
+userName = '잔망루피';
+
+console.log(userName);
+
+// 상수 : 불변의 데이터
+const salerate = 0.2;
+
+// salerate = 0.8;
+console.log(salerate);
+
+// 고전...
+var age = 30;
+
+
+// 이름 관례 (convention)
+// 개발자들끼리의 약속
+
+// 변수, 함수 이름 : camel case
+let MyPhoneNumber = '010-1234-1234'
+
+// 들여쓰기 공백은 2칸
+if (true) {
+    if (true) {
+        console.log('hello!');
+    }
+}
