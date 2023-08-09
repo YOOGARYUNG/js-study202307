ec2385338279b7a046449ce52d3da1c427360e94
GaRyung <shubom0115@naver.com>
2023-08-09 13:33:05 +0900

    ajax - 종합실습: 일정관리프로그램 - 할일 수정

--- 3. ajax/MobieAPI/assets/scripts/app.js
+++ 3. ajax/MobieAPI/assets/scripts/app.js
@@ -67,16 +67,16 @@ const addTodoHandler = e => {
 const $addBtn = document.getElementById('add');
 $addBtn.addEventListener('click', addTodoHandler);
 
-// step2. 할 일 삭제 기능 
+// step3. 할 일 삭제 기능
 const deleteTodoHandler = e => {
   if (!e.target.matches('.remove span')) return;
 
   if (!confirm('진짜로 삭제합니까??')) return;
-  // console.log('삭제 클릭!');
-  // 특정 할일을 지우기 위해 클릭한 할일의 id값을 알아야 함
-  const id = e.target.closest('.todo-list-item').dataset.id
-  // console.log(id);
 
+  // 특정 할일을 지우기 위해 클릭한 할일의 id값을 알아야 함
+  const id = e.target.closest('.todo-list-item').dataset.id;
+  // console.log(id);
+  
   // 서버에 삭제 요청하기
   fetchTodos(`${URL}/${id}`, 'DELETE')
     .then(res => {
@@ -85,10 +85,11 @@ const deleteTodoHandler = e => {
       } else {
         console.log('삭제 실패!');
       }
-    })
+    });
 };
 
 $todoList.addEventListener('click', deleteTodoHandler);
+
 
 // step4. 할 일 완료 체크 처리
 const checkTodoHandler = e => {
@@ -96,10 +97,10 @@ const checkTodoHandler = e => {
 
   // 1. 서버에 수정요청 보내서 누른 그 할일의 
   //    done을 반대값으로 수정해야 함.
-  // 1-1. 현재 체크삾인 t, f인지 알아야 반대로 바꿀수 있음
+  // 1-1. 현재 체크값인 t, f인지 알아야 반대로바꾸지
   console.log(e.target.checked); // 현재상태지 이전상태가 아니다
 
-  const id = e.target.closest('.todo-list-item').dataset.id
+  const id = e.target.closest('.todo-list-item').dataset.id;
   fetchTodos(`${URL}/${id}`, 'PATCH', {
     done: e.target.checked
   });
@@ -108,6 +109,47 @@ const checkTodoHandler = e => {
 $todoList.addEventListener('change', checkTodoHandler);
 
 
+// step5. 할일 수정 처리
+
+// 수정 모드 진입하는 함수
+const enterModifyMode = ($undo) => {
+  // 클래스 이름을 변경하여 아이콘을 바꾸자
+  // -> 클릭한 span태그 노드를 가져와야 함.
+  $undo.classList.replace('lnr-undo', 'lnr-checkmark-circle');
+
+  // $undo근처에 있는 span.text를 가져와야 함.
+  const $textSpan = $undo.closest('.todo-list-item').querySelector('.text');
+  
+  // 교체할 input을 생성
+  const $modInput = document.createElement('input');
+  $modInput.classList.add('modify-input');
+  $modInput.setAttribute('type', 'text');
+  $modInput.value = $textSpan.textContent;
+
+  // span을 input으로 교체하기
+  const $label = $textSpan.parentNode;
+  $label.replaceChild($modInput, $textSpan);
+};
+
+const modifyTodo = ($checkMark) => {
+  const $li = $checkMark.closest('.todo-list-item');
+  const id = $li.dataset.id;
+  const newText = $li.querySelector('.modify-input').value;
+  
+  fetchTodos(`${URL}/${id}`, 'PATCH', {
+    text: newText
+  });
+};
+
+// 수정 이벤트 처리 핸들러
+const modifyTodoHandler = e => {
+  if (e.target.matches('.modify span.lnr-undo')) {
+    enterModifyMode(e.target); // 수정 모드 진입하기
+  } else if (e.target.matches('.modify span.lnr-checkmark-circle')) {
+    modifyTodo(e.target); // 서버에 수정 요청 보내기
+  }
+};
+$todoList.addEventListener('click', modifyTodoHandler);
 
 // =========== 앱 실행 =========== //
 const init = () => {
